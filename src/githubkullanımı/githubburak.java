d377b828893fb6e47a3831f45de94a622f291d99
Burak Özden <burakozden.eem@gmail.com>
2022-09-09 23:41:58 +0300

    versiyon güncelhali

--- src/githubkullanımı/githubburak.java
+++ src/githubkullanımı/githubburak.java
@@ -3,5 +3,6 @@ package githubkullanımı;
 public class githubburak {
     public static void main(String[] args) {
         System.out.println("burakk");
+        System.out.println("fatımaaa");
     }
 }
