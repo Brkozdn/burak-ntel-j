61d5fb0249c11a6d9578186dfc09c98923ea6efd
Burak Özden <burakozden.eem@gmail.com>
2022-09-11 12:29:21 +0300

    burak kontrol müh

---
+++ src/githubkullanımı/özellik10.java
@@ -0,0 +1,22 @@
+package githubkullanımı;
+
+public class özellik10 {
+
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+
+
+        System.out.println("burak kontrol mühendisi");
+
+
+
+
+        System.out.println("burak doktor degil");
+    }
+}
